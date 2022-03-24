a70268110540657f88e3284bd94b3199df1cfe61
prat1k-gupta <pratikgupta9773@gmail.com>
2022-03-24 22:58:10 +0530

    created my own server at port 3000

---
+++ my-express-server/sever.js
@@ -0,0 +1,6 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+app.listen(3000, function(){
+    console.log("server started at port 3000");
+});
--- nodeIntro/index.js
+++ nodeIntro/index.js
@@ -1,3 +1,4 @@
 //jshint esversion: 6
-const fs = require('fs');
-fs.copyFileSync("file1.txt","file2.txt");
+const supervillains = require('supervillains');
+var names = supervillains.random(); 
+console.log(names);
